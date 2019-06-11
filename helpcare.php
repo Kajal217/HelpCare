7286fc66a909b1e96ee4a41e16a4cd7f0c2c734f
Kajal Sheth <ks4vp@virginia.edu>
2019-06-11 18:30:02 -0400

    login styled

---
+++ helpcare.php
@@ -0,0 +1,160 @@
+<!-- // Kajal Sheth
+-->
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">  
+        
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" /> 
+
+   
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <style>
+    .jumbotron{
+      
+      background: #F8F8FF;
+      color: black;
+      text-align: center;
+
+    }
+    .jumbotron p{
+      color: black;
+
+    }
+  </style>
+          
+        
+  </head>
+
+  <body>
+
+    
+     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+     	<a class= "navbar-brand"  > HelpCare </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">   
+          <ul class="navbar-nav">
+            <?php
+              session_start();
+
+
+               if (isset($_SESSION['user'])) {
+                 #is there is cookie/there is a session, then the user has the ability to logout
+               ?>
+                <li class="nav-item"> 
+              <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+                <li class = 'nav-item'>
+                <a class = 'nav-link' href ='profile.php'>Profile</a>
+                <li class = 'nav-item'>
+                <a class = 'nav-link' href ='login.php'>Login</a>
+                </li> 
+              
+<?php
+}
+                else {
+                  #if there is no session, there is no cookie saved, the user can still login
+                 ?>
+
+
+                
+            <li class= "nav-item">
+              <a class= "nav-link" href= "search.php">Search</a>
+                </li>
+              <li class="nav-item"> 
+              <a class="nav-link" href="http://localhost:4200">Contact</a>
+            </li>
+            <li class = 'nav-item'>
+                <a class = 'nav-link' href ='login.html'>Log In</a>
+              </li>
+                                       
+        <?php
+        } ?>    
+            
+            
+          </ul>
+        </div>  
+      </nav>
+
+ 
+    <div class= "jumbotron">
+      <div class = "container">
+        <h1> Connecting People</h1>
+        <p> We can help connect you to your best options! </p>
+        <p> 
+          <a class= "btn btn-primary btn-lg" href= "search.php" role= "button"> Search For A Provider </a>   <!-- this button redirects to search page --> 
+        </p>
+      </div>
+    </div>
+     <div class= "container">
+      <div class= "row">
+          
+         
+
+        <div class= "col-md-4", style= "float:left">   <!-- looks better when floating to the left in terms of spacing --> 
+          <h3> Add A Professor </h3>
+          <p> Don't see one of your professors? Add them to a specific university.</p>
+          <a class= "btn btn-primary btn-lg" href= "ratings.html" role= "button"> Add a Professor </a>
+          
+          <style>
+            
+              .button { 
+                /* button has its own borders and background color. it is centerd in terms of the div*/
+                background-color: #0000FF; 
+                border: none;
+                color: white;
+                padding: 15px 32px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+    }
+          .div{
+            float: left;
+            text-align:center;
+          }
+        </style>
+
+        </div>
+
+        <div class= "col-md-4", style="position:absolute; left:800px; top:375px;">
+          <h3> Add a University</h3>
+          <p> Is your university not registered? Not a problem, register them today! </p>
+          <a class= "btn btn-primary btn-lg" href= "university.html" role= "button"> Add a University </a>
+          <style>
+          .div{
+            float: right;
+            text-align: center;
+          }
+        
+          
+              .button {
+                background-color: #0000FF; 
+                border: none;
+                color: white;
+                padding: 15px 32px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+    }
+   </style>
+        </div>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link type="text/css" rel="stylesheet" href="main_styles.css" />
+    <script src="./Bootstrap example_files/jquery.min.js"></script>
+    <script src="./Bootstrap example_files/bootstrap.min.js"></script>
+    <!-- <script src="js/bootstrap.min.js"></script> if you downloaded bootstrap to your computer -->
+    
+  
+</body></html>
---
+++ login.php
@@ -0,0 +1,110 @@
+<!-- // Kajal Sheth
+-->
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">  
+        
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" /> 
+    <title>Login</title>
+    <link rel="shortcut icon" href="../favicon.ico"> 
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script src="js/modernizr.custom.63321.js"></script>
+
+   
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<style>
+			@import url(http://fonts.googleapis.com/css?family=Ubuntu:400,700);
+			body {
+				background: #563c55 url(images/blurred.jpg) no-repeat center top;
+				-webkit-background-size: cover;
+				-moz-background-size: cover;
+				background-size: cover;
+			}
+			.container > header h1,
+			.container > header h2 {
+				color: #fff;
+				text-shadow: 0 1px 1px rgba(0,0,0,0.7);
+			}
+		</style>
+    <style>
+    .jumbotron{
+      
+      background: #F8F8FF;
+      color: black;
+      text-align: center;
+
+    }
+    .jumbotron p{
+      color: black;
+
+    }
+  </style>
+          
+        
+  </head>
+
+  <body>
+
+    
+     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+     	<a class= "navbar-brand"  > HelpCare </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">   
+          <ul class="navbar-nav">
+
+                <li class="nav-item"> 
+              <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+                <li class = 'nav-item'>
+                <a class = 'nav-link' href ='profile.php'>Profile</a>
+                <li class = 'nav-item'>
+                <a class = 'nav-link' href ='login.php'>Logout</a>
+                </li> 
+
+            
+            
+          </ul>
+        </div>  
+      </nav>
+
+      <section class="main">
+				<form class="form-3">
+				    <p class="clearfix">
+				        <label for="login">Username</label>
+				        <input type="text" name="login" id="login" placeholder="Username">
+				    </p>
+				    <p class="clearfix">
+				        <label for="password">Password</label>
+				        <input type="password" name="password" id="password" placeholder="Password"> 
+				    </p>
+				    <p class="clearfix">
+				        <input type="checkbox" name="remember" id="remember">
+				        <label for="remember">Remember me</label>
+				    </p>
+				    <p class="clearfix">
+				        <input type="submit" name="submit" value="Sign in">
+				    </p>       
+				</form>​
+			</section>
+
+ 
+
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link type="text/css" rel="stylesheet" href="main_styles.css" />
+    <script src="./Bootstrap example_files/jquery.min.js"></script>
+    <script src="./Bootstrap example_files/bootstrap.min.js"></script>
+    <!-- <script src="js/bootstrap.min.js"></script> if you downloaded bootstrap to your computer -->
+    
+  
+</body></html>
